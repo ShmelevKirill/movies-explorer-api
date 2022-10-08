98803dac8190b0dec8e248aaec4a6cbe2064623f
Kirill Shmelev <kirillshmelev1@gmail.com>
2022-10-09 01:28:18 +0300

    cors

--- app.js
+++ app.js
@@ -1,44 +1,44 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const helmet = require('helmet');
-const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
+const helmet = require('helmet');
 const cors = require('./middlewares/cors');
-const router = require('./routes/index');
-const limiter = require('./utils/limiter');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { errorsHandler } = require('./middlewares/errorsHandler');
+const limiter = require('./utils/limiter');
+const router = require('./routes');
+const { PORT, MONGO_URL } = require('./utils/config');
 
 const app = express();
-const { PORT = 3000, MONGO_URL, NODE_ENV } = process.env;
-app.use(bodyParser.json());
-
-mongoose.connect(NODE_ENV === 'production' ? MONGO_URL : 'mongodb://localhost:27017/moviesdb');
 
 app.use(requestLogger);
+
 app.use(helmet());
+
 app.use(limiter);
 
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
-
 app.use(cors);
+
 app.use(router);
+
 app.use(errorLogger);
+
 app.use(errors());
 
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res.status(statusCode)
-    .send({
-      message: statusCode === 500
-        ? 'error server'
-        : message,
-    });
-  next();
-});
+app.use(errorsHandler);
 
-app.listen(PORT);
+mongoose
+  .connect(MONGO_URL, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log('Connected to db');
+  })
+  .catch(() => {
+    console.log('Error to db connection');
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server listen on ${PORT}`);
+});
