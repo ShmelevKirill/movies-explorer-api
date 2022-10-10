558ebc2f169417722efb6d95ba8212f0581e63c2
Kirill Shmelev <kirillshmelev1@gmail.com>
2022-10-11 00:00:28 +0300

    d

--- models/movie.js
+++ models/movie.js
@@ -1,74 +1,68 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-const movieSchema = new mongoose.Schema(
-  {
-    country: {
-      type: String,
-      required: true,
-    },
-    director: {
-      type: String,
-      required: true,
-    },
-    duration: {
-      type: Number,
-      required: true,
-    },
-    year: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    image: {
-      type: Object,
-      required: true,
-      validate: {
-        validator: validator.isURL,
-      },
-    },
-    trailerLink: {
-      type: String,
-      required: true,
-      validate: {
-        validator: validator.isURL,
-      },
-    },
-    thumbnail: {
-      type: Object,
-      required: true,
-      validate: {
-        validator: validator.isURL,
-      },
-    },
-    nameRU: {
-      type: String,
-      required: true,
-    },
-    nameEN: {
-      type: String,
-      required: true,
-    },
-    movieId: {
-      type: Number,
-      required: true,
-    },
-    owner: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'user',
-      required: true,
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: [true, 'Поле должно быть заполнено'],
+  },
+  director: {
+    type: String,
+    required: [true, 'Поле должно быть заполнено'],
+  },
+  duration: {
+    type: Number,
+    required: [true, 'Поле должно быть заполнено'],
+  },
+  year: {
+    type: Number,
+    required: [true, 'Поле должно быть заполнено'],
+  },
+  description: {
+    type: String,
+    required: [true, 'Поле должно быть заполнено'],
+  },
+  image: {
+    type: String,
+    required: [true, 'Поле должно быть заполнено'],
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Неправильный формат ссылки',
     },
   },
-  {
-    versionKey: false,
+  trailerLink: {
+    type: String,
+    required: [true, 'Поле должно быть заполнено'],
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Неправильный формат ссылки',
+    },
   },
-);
+  thumbnail: {
+    type: String,
+    required: [true, 'Поле должно быть заполнено'],
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Неправильный формат ссылки',
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    type: Number,
+    required: [true, 'Поле должно быть заполнено'],
+  },
+  nameRU: {
+    type: String,
+    required: [true, 'Поле должно быть заполнено'],
+  },
+  nameEN: {
+    type: String,
+    name: [true, 'Поле должно быть заполнено'],
+  },
+}, { versionKey: false });
 
-movieSchema.index({ owner: 1, movieId: 1 }, { unique: true });
-
-const Movie = mongoose.model('movie', movieSchema);
-
-module.exports = { Movie };
+module.exports = mongoose.model('movie', movieSchema);
